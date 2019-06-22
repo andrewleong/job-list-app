82e2af41f3853e1cfebeeb18c48a007fef311d53
andrew leong <andrew.leong@jewelpaymentech.com>
2019-06-22 19:58:51 +0800

    Add list of job

---
+++ src/components/JobItem/JobItem.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const JobItem = (props) => {
+    return (
+        <div>
+            <h4>item</h4>
+        </div>
+    );
+};
+
+export default JobItem;
--- src/components/JobList/JobList.jsx
+++ src/components/JobList/JobList.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import JobItem from '../JobItem';
+
 const JobList = ({ jobs }) => {
+    const getJobItem = (jobs) => jobs.map((job, index) => {
+        return <JobItem key={index} job={job} />
+    });
     return (
         <div>
             <h4>XXX jobs found</h4>
+            {getJobItem(jobs)}
         </div>
     );
 };
