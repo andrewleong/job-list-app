38a91740df0cbb1d4c0f686a0f7d6c8bfdfe3dbc
andrew leong <andrew.leong@jewelpaymentech.com>
2019-06-22 22:10:04 +0800

    Style job item

--- src/components/JobItem/JobItem.jsx
+++ src/components/JobItem/JobItem.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
+import { MdLocationOn } from 'react-icons/md';
+import { FaBriefcase, FaGraduationCap, FaClock } from 'react-icons/fa';
 import './JobItem.css'
 
 const JobItem = ({ job }) => {
     const {
         job_title,
         degree,
-        job_country,
         job_type,
         job_location,
         salary_range_from,
@@ -25,17 +26,31 @@ const JobItem = ({ job }) => {
 
     return (
         <div className="job-item-wrapper">
-            <h4>{job_title}</h4>
-            <div>${reformatSalary(salary_range_from)} - ${reformatSalary(salary_range_to)}</div>
-            <div>
-                <span>{job_location}</span>
-                <span>{xp_lvl}</span>
-                <span>{degree}</span>
-                <span>{job_type}</span>
+            <div className="top-row">
+                <h4>{job_title}</h4>
+                <div>${reformatSalary(salary_range_from)} - ${reformatSalary(salary_range_to)}</div>
             </div>
-            <div>
+            <div className="middle-row">
+                <span>
+                    {<MdLocationOn className="icon" />}
+                    {job_location}
+                </span>
+                <span>
+                    {<FaBriefcase className="icon" />}
+                    {xp_lvl}
+                </span>
+                <span>
+                    {<FaGraduationCap className="icon" />}
+                    {degree}
+                </span>
+                <span>{
+                    <FaClock className="icon" />}
+                    {job_type}
+                </span>
+            </div>
+            <div className="bottom-row">
                 <div>{company_name}</div>
-                <div>{company_location}</div>
+                <span>{company_location}</span>
             </div>
         </div>
     );
