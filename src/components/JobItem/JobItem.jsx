b64baaeba30e711a6bdf3b2933265d9ac582d40b
andrew leong <andrew.leong@jewelpaymentech.com>
2019-06-22 21:02:37 +0800

    Reformat salary

--- src/components/JobItem/JobItem.jsx
+++ src/components/JobItem/JobItem.jsx
@@ -1,11 +1,42 @@
 import React from 'react';
 
+import './JobItem.css'
+
 const JobItem = ({ job }) => {
-    // const {
-    // } = job;
+    const {
+        job_title,
+        degree,
+        job_country,
+        job_type,
+        job_location,
+        salary_range_from,
+        salary_range_to,
+        company_name,
+        company_location,
+        xp_lvl,
+    } = job;
+
+    const reformatSalary = (salary) => {
+        const result = Math.abs(salary) > 999 ?
+            Math.sign(salary)*((Math.abs(salary)/1000).toFixed(1)) + 'k'
+            : Math.sign(salary)*Math.abs(salary)
+        return result;
+    }
+
     return (
-        <div>
-            <h4>item</h4>
+        <div className="job-item-wrapper">
+            <h4>{job_title}</h4>
+            <div>${reformatSalary(salary_range_from)} - ${reformatSalary(salary_range_to)}</div>
+            <div>
+                <span>{job_location}</span>
+                <span>{xp_lvl}</span>
+                <span>{degree}</span>
+                <span>{job_type}</span>
+            </div>
+            <div>
+                <div>{company_name}</div>
+                <div>{company_location}</div>
+            </div>
         </div>
     );
 };
