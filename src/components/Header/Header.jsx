41551067be8bd17aad2a905a42ab3b6619912a10
andrew leong <andrew.leong@jewelpaymentech.com>
2019-06-22 17:14:16 +0800

    Add search button

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { FaSearch } from 'react-icons/fa';
 import Logo from '../../assets/images/Logo.svg';
 import './Header.css';
 import SearchBar from './SearchBarContainer';
@@ -7,7 +7,10 @@ import SearchBar from './SearchBarContainer';
 const Header = () => (
     <header className="Header">
         <img src={Logo} width="120" alt="Bossjob" />
-        <h3>Search for job title or company name</h3>
+        <div className="search-btn-container">
+            <FaSearch className="search-btn" />
+            <h3>Search for job title or company name</h3>
+        </div>
         <SearchBar />
         <button className="btn">Filter results</button>
     </header>
